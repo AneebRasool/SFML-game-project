929ea3ff8490317e40bbfde8398354f0f50b0845
Aneeb Rasool <aneeb.rasool@outlook.com>
2026-04-12 21:42:43 -0400

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+
+//using namespace sf (namespace) but here we wont. also test test test
+using namespace std;
+//test!
+
+int main()
+{
+    //window:
+    
+    
+    //Videomode has 
+    sf::RenderWindow window(sf::VideoMode(640, 480), "My first game", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev; //ex event if u press a button, it will find and capture the event in this!
+
+
+    //game loop
+    while (window.isOpen()) {
+
+        //event polling
+        while (window.pollEvent(ev)) { //while we get events from window, save to event variable!
+
+            switch (ev.type) {
+
+            //window close
+            case sf::Event::Closed: window.close(); break;
+
+            case sf::Event::KeyPressed: if (ev.key.code == sf::Keyboard::Escape) {
+                window.close(); break;
+            } 
+
+            
+            }
+            //update
+            
+
+
+            //render
+            window.clear(sf::Color::Blue); //clear old frame
+
+            //draw your game
+            window.display(); //tell app that window is done drawing
+
+
+        }
+    }
+
+    //end of application
+    return 0;
+}
